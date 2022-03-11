5969756ae7caa6934b4be4383e10130b621d8854
박진우 <bas4432@github.com>
2022-03-11 18:40:53 +0900

    feature

--- src/test/java/com/example/demo/CalendarProjectApplicationTests.java
+++ src/test/java/com/example/demo/CalendarProjectApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
@@ -15,48 +16,36 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.demo.Util.DataVO;
-
-
+import com.example.demo.service.Service;
 
 @SpringBootTest
 class CalendarProjectApplicationTests {
 	
-
-	@DisplayName("반환 성공")
-	@Test
-	public void commonGetTest(DataVO dataVO) throws Exception {
-
-		
-		dataVO.setStartDay("2011-01-01");
-		dataVO.setEndDay("2011-03-03");
-		
-		final String DATE_PATTERN = "yyyy-MM-dd";
-		
-         SimpleDateFormat sdf = new SimpleDateFormat(DATE_PATTERN);
-		
-		Date startDate = sdf.parse(dataVO.getStartDay());
-		Date endDate = sdf.parse(dataVO.getEndDay());
-
-		ArrayList<String> dates = new ArrayList<String>();
-		
-		Date currentDate = startDate;
-		
-		
-
-		while (currentDate.compareTo(endDate) <= 0) {
-			dates.add(sdf.format(currentDate));
-			Calendar c = Calendar.getInstance();
-			c.setTime(currentDate);
-			c.add(Calendar.DAY_OF_MONTH, 1);
-			currentDate = c.getTime();
-		}
-		
-		
-		
-		
 	
+	@Autowired
+	Service service;
+
+	
+	@Test
+	public void commonGetTest() throws Exception {
+       
+	    //given
 		
+		
+		//when
+		final int result =  service.multiply(2,3);
+		final int result2 = service.multiply(4,7);
+
+	    System.out.println("result::" + result);
+		System.out.println("result::" + result2);
+		
+	    //then
+	    assertThat(result).isEqualTo(6);
+	    assertThat(result2).isEqualTo(28);
+
 	}
+	
+	 
 	
 	
 
