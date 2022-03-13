70cdba4bc77df4ee26e68cd444c747de4aa4b3cc
박진우 <bas4432@github.com>
2022-03-13 22:51:33 +0900

    여백 재정리

--- src/main/java/com/example/demo/BackEnd/ApiController.java
+++ src/main/java/com/example/demo/BackEnd/ApiController.java
@@ -27,7 +27,6 @@ public class ApiController {
     public List<String> calendar(HttpServletRequest reqeust, DataVO datavo) throws Exception {
 
         final String DATE_PATTERN = "yyyy-MM-dd";
-
         SimpleDateFormat format = new SimpleDateFormat(DATE_PATTERN);
 
         Date startDate = format.parse(datavo.getStartDay());
@@ -37,16 +36,13 @@ public class ApiController {
         calenar.setTime(startDate);
 
         ArrayList<String> dates = new ArrayList<String>();
-
         Date currentDate = startDate;
 
         while (currentDate.compareTo(endDate) <= 0) {
 
             dates.add(format.format(currentDate));
-
             Calendar calendar = Calendar.getInstance();
             calendar.setTime(currentDate);
-
             calendar.add(Calendar.DAY_OF_MONTH, 1);
             currentDate = calendar.getTime();
         }
